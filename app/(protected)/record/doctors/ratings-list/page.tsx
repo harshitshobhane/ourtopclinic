f921acdc219459987465a13b73a6d0ee7eaa1f09
Harshit shobhane <harshit.2022ug1047@iiitranchi.ac.in>
2025-05-16 17:58:20 +0530

    commit error

--- app/(protected)/record/doctors/ratings-list/page.tsx
+++ app/(protected)/record/doctors/ratings-list/page.tsx
@@ -1,14 +1,11 @@
 import { getDoctorRatings } from "@/utils/services/rating";
 import { RatingsListClient } from "./ratings-list-client";
 
-interface RatingsListPageProps {
-  params: {
-    id: string;
-  };
-}
-
-export default async function RatingsListPage({ params }: RatingsListPageProps) {
+const RatingsListPage = async (props: { params: { id: string } }) => {
+  const { params } = props;
   const ratings = await getDoctorRatings(params.id);
 
   return <RatingsListClient ratings={ratings} />;
-} 
+};
+
+export default RatingsListPage; 
