c5f0459d327e5f90fd9c9fbb1000f62308f5aee4
Harshit shobhane <harshit.2022ug1047@iiitranchi.ac.in>
2025-05-22 17:16:42 +0530

    m

--- next.config.js
+++ next.config.js
@@ -3,6 +3,13 @@ const nextConfig = {
   images: {
     domains: ['images.unsplash.com', 'randomuser.me'],
   },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': '.',
+    };
+    return config;
+  },
 }
 
 module.exports = nextConfig 
